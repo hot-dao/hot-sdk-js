153a248e0d55c00e931f003f66c94cd2f74615d3
AZbang <azbangwtf@ya.ru>
2025-02-05 20:43:49 +0300

    1.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import "./adapter/ton";
-
 export type { HotRequest, HotResponse, InjectedState } from "./helpers/types";
 export { default as HOT, RequestFailed } from "./hot";
-export { HotWalletAdapter } from "./adapter/solana";
-export { hotProvider } from "./adapter/evm";
-export { setupHotWallet } from "./adapter/near";
 export {
   verifySignature,
   AuthPayload,
-  authPayloadSchema,
   SignMessageOptionsNEP0413,
   SignedMessageNEP0413,
+  authPayloadSchema,
 } from "./helpers/nep0314";
